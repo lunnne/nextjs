4bfdc5fb70e98089ec9ad0fee5bd19310411aec7
Jin <munjin1340@gmail.com>
2023-12-07 18:17:31 +0100

    Loading Ui, Error Ui

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import Link from 'next/link';
 import { getProducts } from '@/service/products';
 import MeowArticle from '@/components/MeowArticle';
+import clothesImage from '../../../public/images/clothes.jpg'
+import Image from 'next/image';
 
 // export const revalidate = 3;
 
@@ -11,6 +13,7 @@ export default async function ProductsPage() {
   return (
     <>
       <div>제품 소개 페이지</div>
+      <Image src={clothesImage} alt ='clothes'/>
       <ul>
         {products.map((product, index) => (
           <li key={index}>
